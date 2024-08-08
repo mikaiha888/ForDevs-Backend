6ebdccf396e2ccbefbeb531573ab76237f0e043f
Mika <test@Tests-MacBook-Pro.local>
2024-08-08 14:36:51 -0300

    feat(user): add TypeORM config and user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class AppModule {}
