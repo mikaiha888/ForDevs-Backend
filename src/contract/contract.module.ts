6ce54062dbfd8690a004763d259f37e4b2fdd677
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-08-12 11:44:07 -0300

    feat(contract): add CRUD operations for contracts

--- src/contract/contract.module.ts
+++ src/contract/contract.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ContractService } from './contract.service';
 import { ContractController } from './contract.controller';
+import { Contract } from './entities/contract.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Contract])],
   controllers: [ContractController],
   providers: [ContractService],
 })
