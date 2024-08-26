f0bc227127db32af8413e92696a75be91c4ce299
Mika <test@Tests-MacBook-Pro.local>
2024-08-26 14:53:07 -0300

    feat: add product entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,29 +7,31 @@ import { UserModule } from './modules/user/user.module';
 import { LikeModule } from './modules/like/like.module';
 import { LinkModule } from './modules/link/link.module';
 import { PlanModule } from './modules/plan/plan.module';
+import { RoleModule } from './modules/role/role.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ReviewModule } from './modules/review/review.module';
 import { ProjectModule } from './modules/project/project.module';
+import { ProductModule } from './modules/product/product.module';
 import { ContractModule } from './modules/contract/contract.module';
 import { TechnologyModule } from './modules/technology/technology.module';
 import { CommissionModule } from './modules/commission/commission.module';
-import { RoleModule } from './modules/role/role.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(dataSourceOptions),
     TagModule,
     UserModule,
+    PlanModule,
+    RoleModule,
+    LinkModule,
     LikeModule,
+    AuthModule,
     ReviewModule,
+    ProductModule,
     ProjectModule,
     ContractModule,
     TechnologyModule,
     CommissionModule,
-    LinkModule,
-    PlanModule,
-    AuthModule,
-    RoleModule,
   ],
 })
 export class AppModule {}
