98e407c9182890f167b216ef9fec7a5c56737081
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-08-29 15:47:08 -0300

    feat swagger tags for routes

--- src/modules/core/payment/payment.controller.ts
+++ src/modules/core/payment/payment.controller.ts
@@ -1,31 +1,38 @@
 import { Controller, Post, Body,} from '@nestjs/common';
 import { PaymentService } from './payment.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('payments')
+@ApiTags('Payments')
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
   @Post('mercadopago/create')
+  @ApiOperation({summary: 'create mercadopago link'})
   createMercadoPagoPreference(@Body() createPaymentDto: any) {
     return this.paymentService.createMercadoPagoPreference(createPaymentDto);
   }
 
   @Post('mercadopago/notification')
+  @ApiOperation({summary: 'update user to premium'})
     handleMercadoPagoNotification(@Body() paymentData: any) {
     return this.paymentService.handleMercadoPagoNotification(paymentData);
   }
 
   @Post('stripe/create')
+  @ApiOperation({summary: 'create stripe link'})
   createStripeSession(@Body() createPaymentDto: any) {
     return this.paymentService.createStripeSession(createPaymentDto);
   }
 
   @Post('stripe/notification')
+  @ApiOperation({summary: 'update user to premium'})
   async handleStripePayment(@Body() body: { sessionId: string }) {
     return this.paymentService.handleStripePayment(body.sessionId);
   }
 
   @Post('subscription/cancel')
+  @ApiOperation({summary: 'cancel subscription'})
   cancelSubscription(
     @Body('userId') userId: string,
     @Body('subscriptionId') subscriptionId: string,
--- src/modules/core/plan/plan.controller.ts
+++ src/modules/core/plan/plan.controller.ts
@@ -3,32 +3,39 @@ import { PlanService } from './plan.service';
 import { CreatePlanDto } from './dto/create-plan.dto';
 import { UpdatePlanDto } from './dto/update-plan.dto';
 import { Name } from './entities/plan.entity';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('plan')
+@ApiTags('Plan')
 export class PlanController {
   constructor(private readonly planService: PlanService) {}
 
   @Post()
+  @ApiOperation({summary: 'create plan'})
   create(@Body() createPlanDto: CreatePlanDto) {
     return this.planService.create(createPlanDto);
   }
 
   @Get()
+  @ApiOperation({summary: 'get all plans'})
   findAll() {
     return this.planService.findAll();
   }
 
   @Get(':name')
+  @ApiOperation({summary: 'get plan by name'})
   findOne(@Param('name') name: Name) {
     return this.planService.findOne(name);
   }
 
   @Patch(':name')
+  @ApiOperation({summary: 'update plan'})
   update(@Param('name') name: Name, @Body() updatePlanDto: UpdatePlanDto) {
     return this.planService.update(name, updatePlanDto);
   }
 
   @Delete(':name')
+  @ApiOperation({summary: 'delete plan'})
   remove(@Param('name') name: Name) {
     return this.planService.remove(name);
   }
